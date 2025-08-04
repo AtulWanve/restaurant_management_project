adf1ee4fbad4b79a81fbdea1ecdbe2109bc2ee54
AtulWanve <150960955+AtulWanve@users.noreply.github.com>
2025-08-04 12:45:37 +0530

    16640

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    # stores overall details of an order
+    customer_name = models.CharField(max_length=100)
+    note = models.TextField(blank=True, null=True)
+    total_order_amount = models.FloatField()
+    is_paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order #{self.id} by {self.customer_name}'
+
+class OrderItem(models.Model):
+    # stores individual items in an order
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    item_name = models.CharField(max_length=100)
+    item_price = models.FloatField()
+    qnty = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.qnty} x {self.item_name} (Order #{self.order.id})'
