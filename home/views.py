36fe7b8b35346594a03637712018c649621fa15c
AtulWanve <150960955+AtulWanve@users.noreply.github.com>
2025-08-10 08:14:00 +0530

    26335

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.conf import settings
 
 def homepage(request):
+    context = {
+        'restaurant_name': settings.RESTAURANT_NAME
+    }
     return render(request, 'home/index.html')
