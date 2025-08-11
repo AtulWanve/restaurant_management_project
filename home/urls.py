c9e64697d5c2ed70a2566ba5e473edec623da3ea
AtulWanve <150960955+AtulWanve@users.noreply.github.com>
2025-08-11 15:32:25 +0530

    28741

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from home import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('menu-items/', views.MenuItemsView.as_view(), name='menu-items'),
+    path('contact-us', views.contact_us, name='contact-us'),
 ]
