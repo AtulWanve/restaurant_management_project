ecfc0f4c9085bde20d042964d91484acb1575080
AtulWanve <150960955+AtulWanve@users.noreply.github.com>
2025-08-11 12:39:44 +0530

    28739

--- home/views.py
+++ home/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 def homepage(request):
     context = {
         'restaurant_name': settings.RESTAURANT_NAME
     }
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
+
+class MenuItemsView(APIView):
+    def get(self, request):
+        menu_items = [
+            {'id': 1, 'name': 'Pizza', 'price': 9.99},
+            {'id': 2, 'name': 'Burger', 'price': 5.99},
+            {'id': 3, 'name': 'Pasta', 'price': 7.49},
+        ]
+        return Response(menu_items)
