3bf68e5e7e22b1c75436ef7a42de79bbb2d7c3e5
AtulWanve <150960955+AtulWanve@users.noreply.github.com>
2025-08-15 08:06:24 +0530

    31795

--- home/views.py
+++ home/views.py
@@ -2,22 +2,26 @@ from django.shortcuts import render
 from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from datetime import datetime
 
 def homepage(request):
     context = {
         'restaurant_name': settings.RESTAURANT_NAME,
         'restaurant_phone': settings.RESTAURANT_PHONE
     }
-    return render(request, 'home/index.html', context)
+    return render(request, 'home/index.html', context, {'year': current_year})
+
+def current_year(request):
+    return {"year": datetime.now().year}
 
 def contact_us(request):
-    return render(request, 'home/contact.html')
+    return render(request, 'home/contact.html', {'year': current_year})
 
 def about(request):
-    return render(request, 'home/about.html')
+    return render(request, 'home/about.html', {'year': current_year})
 
 def reservations(request):
-    return render(request, 'home/reservations.html')
+    return render(request, 'home/reservations.html', {'year': current_year})
 
 class MenuItemsView(APIView):
     def get(self, request):
