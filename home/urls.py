231da5977327ce243a8695a883a3b6c6e739aa15
AtulWanve <150960955+AtulWanve@users.noreply.github.com>
2025-08-15 07:46:54 +0530

    31794

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('menu-items/', views.MenuItemsView.as_view(), name='menu-items'),
     path('contact-us/', views.contact_us, name='contact-us'),
     path('about/', views.about, name='about'),
+    path('reservations/', views.reservations, name='reservations'),
 ]
