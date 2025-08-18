b37a5a42a39c3665f754d4043d0a7704f009183c
AtulWanve <150960955+AtulWanve@users.noreply.github.com>
2025-08-18 18:18:16 +0530

    33312

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Feedback(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Feedback from {self.name or 'Anonymous' } on {self.created_at.strftime('%Y-%m-%d')}"
+        
