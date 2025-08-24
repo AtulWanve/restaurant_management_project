95825fda90b172895e7c6034cdd62fa530bda6c3
AtulWanve <150960955+AtulWanve@users.noreply.github.com>
2025-08-24 22:41:54 +0530

    37932

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'status', 'created_at']
+    list_filter = ['status', 'created_at']
+    search_fields = ['user__username']
+
+admin.site.register(Order, OrderAdmin)
