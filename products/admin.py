0c6480560482fd2505b64c58411738e451bd7a23
AtulWanve <150960955+AtulWanve@users.noreply.github.com>
2025-08-24 22:29:57 +0530

    37932

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import *
-
+from .models import Item
 
 # Custom Admins
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['item_name','item_price','created_at']
-
+    search_fields = ['item_name']
+    list_filter = ['created_at']
 
 # Register your models here.
-admin.site.register(Item,ItemAdmin)
+admin.site.register(Item, ItemAdmin)
