dc0b5ee769d904fe0f9e4a7a7ad216bd74059a1e
AtulWanve <150960955+AtulWanve@users.noreply.github.com>
2025-08-28 22:39:35 +0530

    38723

--- products/views.py
+++ products/views.py
@@ -25,3 +25,7 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def menu_view(request):
+    items = Item.objects.all()
+    return render(request, 'product')
