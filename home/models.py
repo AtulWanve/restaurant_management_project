23259e4f6f0736b73a6d8b2d445e76bf5a98a87f
AtulWanve <150960955+AtulWanve@users.noreply.github.com>
2025-08-30 15:39:30 +0530

    38720

--- home/models.py
+++ home/models.py
@@ -8,3 +8,11 @@ class Feedback(models.Model):
     def __str__(self):
         return f"Feedback from {self.name or 'Anonymous' } on {self.created_at.strftime('%Y-%m-%d')}"
         
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} ({self.email})'
